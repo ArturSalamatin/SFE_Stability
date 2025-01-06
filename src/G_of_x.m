747213b8f9a4e6200f03135af64a8b2e193fa286
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-01-06 19:36:39 +0300

    Create G_of_x.m

---
+++ src/G_of_x.m
@@ -0,0 +1,14 @@
+function out = G_of_x(x, params)
+a0 = params.a0;
+g0 = params.g0;
+g1 = params.g1;
+
+out= ones(size(x));
+
+mask = x <= a0;
+out(mask) = g0*x(mask);
+
+mask = (x > a0) & (x < 1);
+out(mask) = g0*a0 + g1*(x(mask) - a0);
+
+end
