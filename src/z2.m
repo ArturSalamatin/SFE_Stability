21406cc2a399a7ddc5d304c40c7911718054cef7
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-01-06 19:28:46 +0300

    Update z2.m

--- src/z2.m
+++ src/z2.m
@@ -3,19 +3,13 @@ function out = z2(params)
 a = params.a;
 a0 = params.a0;
 r = params.r;
+g0 = params.g0;
 g1 = params.g1;
 
 if(a < a0)
-    % here z0 does not propagate yet
-    out = ...
-        0;
+    out = a/g0;
 else
-    % here z0 > 0, and popagates
-    out = (a-a0+...
+    out = a0/g0 + (a-a0+...
         r/(1-r)*log((r+(1-r)*a0)/(r+(1-r)*a)))/g1;
 end
-
-
-
-
 end
