c5e5bc3fa83676fdddc98ac60b1c5f19c223b770
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-01-06 19:29:01 +0300

    Update debug.m

--- src/debug.m
+++ src/debug.m
@@ -4,17 +4,33 @@ close all
 
 params.a0 = 0.05;
 params.r = 0.5;
-params.t = (params.a0^2)/2;
-params.a = sqrt(2*params.t);
-params.g0 = 1-params.r + params.r/params.a0;
 params.g1 = 1-params.r;
-params.z0 = z0(params);
-params.z2 = z2(params);
+params.g0 = params.g1 + params.r/params.a0;
 
-x = x_grid(params);
-z = z_of_x(x, params);
 
-plot(z/params.z0,x/params.a)
+figure(100)
+hold on
+axis([0 0.7 0 1])
+box on
+xlabel('z')
+ylabel('X')
+
+t = linspace(1E-2, 0.5, 11);
+for i = 1:numel(t)
+    params.t = t(i);
+    params.a = sqrt(2*params.t);
+    
+    params.z0 = z0(params);
+    params.z2 = z2(params);
+    
+    x = x_grid(params);
+    z = z_of_x(x, params);
+    
+    plot(z, x, 'k-', 'LineWidth', 1)
+end
+
+
+plot([0 0.7], params.a0*[1 1], 'k--')
 
 
 
