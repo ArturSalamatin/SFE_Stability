74917135845e73841079fc6ec9b073b511411757
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-01-07 13:21:50 +0300

    Update z2.m

--- src/z2.m
+++ src/z2.m
@@ -2,14 +2,8 @@ function out = z2(params)
 
 a = params.a;
 a0 = params.a0;
-r = params.r;
 g0 = params.g0;
-g1 = params.g1;
 
-if(a < a0)
-    out = a/g0;
-else
-    out = a0/g0 + (a-a0+...
-        r/(1-r)*log((r+(1-r)*a0)/(r+(1-r)*a)))/g1;
-end
+out = min(a, a0)/g0 + z0(params);
+
 end
