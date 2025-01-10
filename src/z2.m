dc44202fec3bd11ac8ee391f9aa369172e8abe9e
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-01-10 18:59:54 +0300

    changes for the monodisperse packed bed

--- src/z2.m
+++ src/z2.m
@@ -1,9 +1,4 @@
 function out = z2(params)
-
 a = params.a;
-a0 = params.a0;
-g0 = params.g0;
-
-out = min(a, a0)/g0 + z0(params);
-
+out = a;
 end
--- src/z_of_x.m
+++ src/z_of_x.m
@@ -1,27 +1,4 @@
 function out = z_of_x(x, params)
-
-t = params.t;
-a0 = params.a0;
-g0 = params.g0;
-g1 = 1-params.r;
-r = params.r;
-
-a = sqrt(2*t);
-
-if(a < a0)
-    out = (a-x)/g0;
-else
-    out = zeros(size(x));
-    mask = x <= a0;
-    out(mask) = (a0 - x(mask))/g0 + ...
-        (a - a0 + r/(1-r)*log((r+(1-r)*a0)/(r+(1-r)*a)))/g1;
-    
-    mask = (x > a0) & (x <= 1);
-    out(mask) = (a-x(mask)+...
-        r/(1-r)*log((r+(1-r)*x(mask))/(r+(1-r)*a)))/(1-r);
-    
-    mask = x > 1;
-    if(any(mask))
-        error('X must not be greater than 1');
-    end
+a = params.a;
+out = a-x;
 end
