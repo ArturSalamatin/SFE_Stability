a644b6d5c378feef0a64e245160154be6435c93e
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-01-10 19:24:26 +0300

    fix

--- src/debug_sigma.m
+++ src/debug_sigma.m
@@ -12,6 +12,14 @@ params.z2 = z2(params);
 
 params.R = 1;
 params.alpha = 5;
-sigma = 100;
+% sigma = 100;
 
-out = J(2.966645999, params);
+
+sigma = linspace(-40, -0.001, 205);
+out = zeros(size(sigma));
+for i = 1:numel(sigma)
+    out(i) = J(sigma(i), params);
+end
+
+figure(1000)
+plot(sigma, out)
