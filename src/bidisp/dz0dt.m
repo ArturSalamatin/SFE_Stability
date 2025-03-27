98165245fe08cb6c645959019ff18de475609f4e
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-03-27 11:43:29 +0300

    Create dz0dt.m

---
+++ src/bidisp/dz0dt.m
@@ -0,0 +1,15 @@
+function out = dz0dt(params)
+
+a = params.a;
+a0 = params.a0;
+r = params.r;
+
+if(a < a0)
+    % here z0 does not propagate yet
+    out = ...
+        0;
+else
+    % here z0 > 0, and front popagates
+    out = 1/(r+(1-r)*a);
+end
+end
