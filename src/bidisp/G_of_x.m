fdbf34576927eea8b051090f47ce352ad86aff41
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-03-27 11:35:46 +0300

    Create G_of_x.m

---
+++ src/bidisp/G_of_x.m
@@ -0,0 +1,18 @@
+function out = G_of_x(x, params)
+a0 = params.a0;
+a1 = params.a1;
+g0 = params.g0;
+g1 = params.g1;
+
+out = ones(size(x));
+
+mask = x <= a0;
+out(mask) = g0*x(mask);
+
+mask = (x > a0) & (x < a1);
+out(mask) = g0*a0 + g1*(x(mask) - a0);
+
+mask = x >= a1;
+out(mask) = 1; % == (g0-g1)*a0+g1*(a1-a0);
+
+end
