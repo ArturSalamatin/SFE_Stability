01b0e02bddc28147137d9a0d8e8f5fa5a3500f28
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-03-27 13:30:48 +0300

    Update G_div_X.m

--- src/bidisp/G_div_X.m
+++ src/bidisp/G_div_X.m
@@ -2,6 +2,7 @@ function out = G_div_X(x, params)
 % The function calculates the ratio G(X)/X
 r = params.r;
 a0 = params.a0;
+a1 = params.a1;
 g0 = params.g0;
 g1 = params.g1;
 
@@ -11,7 +12,7 @@ for i = 1:numel(x)
         error('X must not be negative!');
     elseif(x(i) <= a0)
         out(i) = g0;
-    elseif(x(i) <= 1)
+    elseif(x(i) <= a1)
         out(i) = r/x(i)+g1;
     else
         out(i) = 1/x(i);
