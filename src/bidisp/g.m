f33d722ea19cac3ac8c38682eadd0d8da5c38168
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-03-29 13:02:05 +0300

    Update g.m

--- src/bidisp/g.m
+++ src/bidisp/g.m
@@ -8,21 +8,22 @@ a1 = params.a1;
 out = zeros(size(x));
 
 for i = 1:numel(out)
-if(x(i) < 0)
-    error('X must be positive!');
-end
-
-
-
-if(x(i) < a0)
-    out(i) = g0
-elseif(x(i) == a0)
-    out(i) = g0-g1;
-elseif(x(i) < a1)
-    out(i) = g1;
-elseif(x(i) == a1)
-    out(i) = g1 - 0;
-else
-    out(i) = 0;
+    if(x(i) < 0)
+        error('X must be positive!');
+    end
+    
+    
+    
+    if(x(i) < a0)
+        out(i) = g0;
+    elseif(x(i) == a0)
+        out(i) = g0-g1;
+    elseif(x(i) < a1)
+        out(i) = g1;
+    elseif(x(i) == a1)
+        out(i) = g1 - 0;
+    else
+        out(i) = 0;
+    end
 end
 end
