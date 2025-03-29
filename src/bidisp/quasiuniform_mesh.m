0faf4282cb79bffd73d34f26df203a483ff53186
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-03-29 10:57:31 +0300

    Update quasiuniform_mesh.m

--- src/bidisp/quasiuniform_mesh.m
+++ src/bidisp/quasiuniform_mesh.m
@@ -1,10 +1,18 @@
-function mesh = quasiuniform_mesh(L, R, N)
+function mesh = quasiuniform_mesh(L, Mid, R, N)
+% set mesh
+% approximate quasi-uniform step
+h = (R-L)/(N-1);
+%% set left mesh segment
+mesh.left = uniform_mesh(L, Mid, ceil((Mid - L)/h) +1);
+%% set left mesh segment
+mesh.right = uniform_mesh(Mid, R, ceil((R - Mid)/h) +1);
+end
+
+function mesh = uniform_mesh(L, R, N)
 % mesh
 mesh.L = L;
 mesh.R = R;
 mesh.N = N;
 mesh.t = linspace(L, R, N);
 mesh.I = 1:(N-1);
-% steps
-% mesh.steps = xi(I+1) - xi(I);
 end
