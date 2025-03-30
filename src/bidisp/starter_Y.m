405cb8f631d47c8edcdc1f2ef6a1186691cc01bd
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-03-30 13:26:32 +0300

    Update starter_Y.m

--- src/bidisp/starter_Y.m
+++ src/bidisp/starter_Y.m
@@ -93,7 +93,7 @@ h = params.h;
 %% BC at the left end
 left = zeros(eqN,eqN);
 left(1,1) = 1; % Phi(0) = 0
-left(2,2) = 1; % Omega(0) = 0 /* = Psi(0)*/
+left(2,2) = 1; % Psi(0) = 0
 % left(3,3) = 1; % Y(0) = 0
 % left(4,4) = 1; % G(0)   = 1
 %% BC at the right end
@@ -101,7 +101,7 @@ right = zeros(eqN,eqN);
 % right(2,2) = 1; % Omega(1) = 0
 right(3,1) = h; % f*Phi(1) + G(1) = 0
 right(3,4) = 1; 
-right(4,4) = 1; % G(1)   = 1
+right(4,4) = 1; % G(1)   = -1
 %% rhs for BC eqns
 out.left = left;
 out.right = right;
