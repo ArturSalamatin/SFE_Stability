678d9a3ee34293a5319ff3ad33e91a3a318f8af2
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-03-31 16:29:19 +0300

    Update fit_sigma.m

--- src/bidisp/fit_sigma.m
+++ src/bidisp/fit_sigma.m
@@ -1,12 +1,21 @@
 function [sigma, sol] = fit_sigma(starter, params, guess)
 if(nargin == 2)
-    guess = make_guess(starter, params);
+    % make guess, if no guess provided for sigma
+    [guess, L, R] = make_guess(starter, params);
 end
 sigma = guess;
-[sigma, val] = ...
+[sigma, val, exitflag, output] = ...
     fzero(@(s) func_to_min(starter, s, params), guess);
-if(abs(val) > 2e-8)
+if(abs(val) > 2e-5)
+    % solution may not be found
     sigma = NaN;
+    if(nargin == 3)
+        % if no guess was constructed internally
+        [guess, L, R] = make_guess(starter, params);
+    end
+    % use segment division by half method
+    [sigma, val] = ...
+        fzero(@(s) func_to_min(starter, s, params), [L,R]);
     %     error('Wrong value found!');
 end
 
@@ -41,8 +50,11 @@ end
 out = i;
 end
 
-function out_I = make_guess(starter, params)
-sigma = linspace(-3,2,101);
+function [out_I, L, R] = make_guess(starter, params)
+a = params.a;
+sigma_min = (-1-a*a*params.dz2dt/params.z2)*1.1;
+
+sigma = linspace(sigma_min,1,81);
 out = zeros(size(sigma));
 for i = 1:numel(sigma)
     sol = starter(sigma(i), params);
@@ -54,6 +66,7 @@ sigma = sigma(idx:end);
 out = out(idx:end);
 
 I = sign_change(out);
-
-out_I = (sigma(I) + sigma(I-1))/2;
+R = sigma(I);
+L = sigma(I-1);
+out_I = (L + R)/2;
 end
