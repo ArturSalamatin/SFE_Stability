6ce1b095403efb923495c8ee38205d362853f8d8
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-11-13 17:11:19 +0300

    fix

--- src/bidisp/test_eigenfuncs.m
+++ src/bidisp/test_eigenfuncs.m
@@ -3,19 +3,20 @@ clear all
 % close all
 
 global sigma_fig
-sigma_fig = 6;
+sigma_fig = 9;
 
 %% packed bed params
 a0 = 0.2;
 alpha = [0, 0.1, 0.3, 0.5, 0.7];
 marker = ['o','s','d','*','x'];
 
-params = poly_case(a0, alpha(4), 0.1);
+params = poly_case(a0, 0.3, 0.5);
 params.marker = marker(2);
 
-N = 900;
-q = 1.06;
-mesh = log_mesh(q, N, params);
+N = 800;
+% q = 1.06;
+% mesh = log_mesh(q, N, params);
+mesh = quasiuniform_mesh(N, params);
 starter = @(sigma, params) starter_Y(...
                 sigma, params, mesh);
 
