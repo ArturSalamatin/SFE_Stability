aa59638e449657bab37cc5f546a4da2c467e1a5f
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-11-13 17:08:52 +0300

    refactored

--- src/bidisp/plot_fig4_sigma.m
+++ src/bidisp/plot_fig4_sigma.m
@@ -46,21 +46,4 @@ for t_id = 1:numel(t)
 end
 
 
-function [params, sigma] = case_monodisp
-params.a0 = 0.5;
-params.a1 = 1.0;
-params.r = 0.0; % dust volume fraction
-params.g1 = (1-params.r)/params.a1;
-params.g0 = params.g1 + params.r/params.a0;
-params.R = 0.5;
-params.h = 5;
-params.t = 0.2;
-params.a = sqrt(2*params.t);
-params.z0 = z0(params);
-params.z2 = z2(params);
-params.dz2dt = dz2dt(params);
-params.C1 = params.a./z2(params);
-params.C2 = params.C1*params.a.*params.dz2dt;
 
-sigma = -1.9072018;
-end
