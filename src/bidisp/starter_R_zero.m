92f9674977135cc7b52d9d00e1ae571c2c5689c4
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-11-14 13:20:47 +0300

    fix: BC method

--- src/bidisp/starter_R_zero.m
+++ src/bidisp/starter_R_zero.m
@@ -86,18 +86,15 @@ d_xi = d_zeta/zeta2;
 y = x*d_xi;
 %% BC at the left end
 left = zeros(eqN,eqN);
-left(1,1) = 1; % Phi(0) = 0
+left(1,1) = 1; % Y(0) = 0
 left(2,2) = 1; % Psi(0) = 0
 %% BC at the right end
 right = zeros(eqN,eqN);
-% right(2,2) = 1; % Omega(1) = 0
-right(3,1) = h; % h*Phi(1) + G(1) = 0
-right(3,4) = 1; 
-right(4,4) = 1; % G(1)   = -1
 %% rhs for BC eqns
+% left*y(0) + right*y(1) = rhs
 out.left = left;
 out.right = right;
-out.rhs = [0;0;0;-1];
+out.rhs = [y;psi];
 end
 
 function out = JC(params, base_state, eqN)
