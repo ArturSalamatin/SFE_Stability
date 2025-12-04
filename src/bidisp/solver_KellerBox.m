600e6f3cb3834094ab614ac7f163719bb64f480e
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-12-04 15:56:57 +0300

    Keller box solver fixed and file renamed

--- src/bidisp/solver_KellerBox.m
+++ src/bidisp/solver_KellerBox.m
@@ -1,16 +1,16 @@
-function sol = solver(problem, mesh)
+function sol = solver_KellerBox(problem, mesh)
 %% ODE solver using Keller box scheme
 eqN = problem.eqN; % nmbr of equations
 ids = problem.ids; % ids within a block, 1:eqN
 M = problem.M; % nmbr of discrete unknows
 xi = mesh.xi;
-segmIds = mesh.segmIds; % jump node is skipped
 %% allocate memory
 A = spalloc(M,M,M*eqN); % matrix
 % A = full(A);
 b = zeros(M, 1); % rhs
 % fill in the matrix
 %% fill regular segments
+segmIds = mesh.segmIds; % jump node is skipped
 for i = segmIds
     block_pos = (i-1)*eqN;
     step = (xi(i+1) - xi(i))/2.0; % (!)trapezoidal rule
@@ -26,7 +26,7 @@ end
 %% jump conditions, if any
 jumpIds = mesh.segmIds; % jump segments, may be empty
 for i = jumpIds
-    block_pos = (mesh.left.N-1)*eqN;
+    block_pos = (i-1)*eqN;
     jc = problem.JC();
     A(block_pos+ids, block_pos+ids) = jc.left;
     A(block_pos+ids, block_pos+eqN+ids) = jc.right;
@@ -34,7 +34,8 @@ for i = jumpIds
 end
 %% boundary conditions
 % BC -- boundary conditions
-block_pos = (mesh.N-1)*eqN;
+N = mesh.N;
+block_pos = (N-1)*eqN;
 bc = problem.BC();
 A(block_pos+ids, ids) = bc.left;
 A(block_pos+ids, block_pos+ids) = bc.right;
