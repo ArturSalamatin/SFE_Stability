62f434855e27f5364b5a70d55b38a1f81b61c0ad
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-12-16 17:53:10 +0300

    calculator interface is introduced

--- src/bidisp/main_Rzero_contplot_alpha_tau.m
+++ src/bidisp/main_Rzero_contplot_alpha_tau.m
@@ -13,70 +13,70 @@ xBarRight = 0*4e-2;
 solver = @(problem, mesh, params) solver_RK(problem, mesh, params);        
 label = '';
 N = 200;
-for Alpha = [5, 2, 4, 6, 8]
-    alpha = Alpha/10;
-    num = 2200+alpha*10;
+for A0 = [3]
+    a0 = A0/10;
+    num = 2200+a0*10;
     clc
     col = {'k'};
     style = {'-'};
     pen = set_pen(col, style);
     %% make calculation grid
     eps = 1e-2;
-    tau0 = linspace(eps, 0.5 - eps, 300);
-    a0 = linspace(eps,1-eps, 300);
-    [tau0, a0] = meshgrid(tau0, a0);
+    tau0 = linspace(a0*a0/2 + eps, 0.5 - eps, 10);
+    alpha = linspace(eps,1-eps, 10);
+    [tau0, alpha] = meshgrid(tau0, alpha);
+    sigma = 0*tau0 -2;
     %% run calculations
 %     [tau0, a0, sigma] = ...
 %         calc_sigma_R_zero(solver, N, alpha, tau0, a0, pen);
-%     save(['R_zero_data/alpha_', num2str(alpha*100), '.mat'], ...
+%     save(['R_zero_data/a0_', num2str(a0*100), '.mat'], ...
 %         'tau0', 'a0', 'sigma', 'alpha')
-load(['R_zero_data/alpha_', num2str(alpha*100), '.mat'], ...
-        'tau0', 'a0', 'sigma', 'alpha')
-    
-    
-    levels = linspace(-2,-1,21);
-    switch Alpha
-            case 2
-            levels = linspace(-2,-1,11);
-    end
+% load(['R_zero_data/alpha_', num2str(alpha*100), '.mat'], ...
+%         'tau0', 'a0', 'sigma', 'alpha')
+
+%     levels = linspace(-2,-1,21);
+%     switch Alpha
+%             case 2
+%             levels = linspace(-2,-1,11);
+%     end
         figure(num)
         hold on
-        axis([0 0.5 0 1])
-        contour(tau0, a0, sigma, ...
-            levels,...
+        axis([floor((a0*a0/2)*10)/10 0.5 0 1])
+        contour(tau0, alpha, sigma, ...
+            ...levels,...
             'ShowText','on',...
             'linecolor', 'black')
     
-        x = linspace(0, 0.5, 700);
-        y = sqrt(2*x);
+        x = [1,1]*a0*a0/2;
+        y = [0,1];
         plot(x,y, 'k-', 'linewidth', 1)
     
-        x = -1:0.1:0;
-        y = -(1+x)*alpha./(x*(1-alpha));%1/(1-alpha)*(alpha./(x+2)-alpha);
-        z = y.*y/2;
-        plot(z,y,'ks', 'MarkerFaceColor', 'black')
+%         x = -1:0.1:0;
+%         y = -(1+x)*alpha./(x*(1-alpha));%1/(1-alpha)*(alpha./(x+2)-alpha);
+%         z = y.*y/2;
+%         plot(z,y,'ks', 'MarkerFaceColor', 'black')
     
         label = '';
-        switch Alpha
-            case 2
+        switch A0
+            case 1
                 label = 'A';
-            case 4
+            case 2
                 label = 'B';
-            case 6
+            case 3
                 label = 'C';
-            case 8
+            case 4
                 label = 'D';
         end
     
         box on
-        set(gca, 'xTick', [0:0.1:0.5])
+%         set(gca, 'xTick', [0:0.1:0.5])
         set(gca, 'yTick', [0:0.2:1.0])
         set(gcf, 'units', 'centimeters', 'OuterPosition', [10.42 6.27 8.5 9])
         set(gca, 'FontSize', 10, 'Position', [0.16, 0.2, 0.78, 0.74])
-        ylabel(['{\ita}_0, ' char(8211)])
+        ylabel(['{\it\alpha}, ' char(8211)])
         xlabel(['{\it\tau}_0, ' char(8211)])
         annotation('textbox',...
-            [0.207441165123454 0.793200250350447 0.115740738211223 0.141129029253798],...
+            [0.845461998456785 0.809174691245016 0.115740738211223 0.141129029253798],...
             'String',{label},...
             'LineStyle','none',...
             'FontSize',16,...
@@ -85,10 +85,10 @@ load(['R_zero_data/alpha_', num2str(alpha*100), '.mat'], ...
         hFig = findobj('Type', 'figure', 'Number', num);
         path = 'R_zero_data/Figs/';
         if(~isempty(hFig))
-            saveas(num, [path, 'fig4', label], 'emf');
-            saveas(num, [path, 'fig4', label], 'eps');
-            saveas(num, [path, 'fig4', label], 'fig');
-            saveas(num, [path, 'fig4', label], 'png');
+            saveas(num, [path, 'fig5', label], 'emf');
+            saveas(num, [path, 'fig5', label], 'eps');
+            saveas(num, [path, 'fig5', label], 'fig');
+            saveas(num, [path, 'fig5', label], 'png');
         end
 end
             
