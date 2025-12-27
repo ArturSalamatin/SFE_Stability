f382281118da87dfc5f59809cefae78536e6a132
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-12-27 21:53:51 +0300

    bugfix

--- src/bidisp/calc_full_inlet_solution_asymptotics.m
+++ src/bidisp/calc_full_inlet_solution_asymptotics.m
@@ -37,12 +37,12 @@ lambda0 = a*a*(2+sigma);
 lambda1 = a*a/D*y2+2*a*(1+sigma);
 lambda2 = 1+sigma;
 
-phi1 = 0;
+phi1 = a*gamma0/(D*zeta2*(r+1));
 gamma1 = 0;
 psi1 = -g1/(D*(r+1))*(phi0+b0);
 b1 = (a*a*psi1 - (C2*l2*r+lambda1)*b0)/(C2*l1*(r+1)-lambda0);
 
-phi2 = 0;
+phi2 = (a*gamma1-gamma0+zeta2*y2*(r+1)*phi1)/(zeta2*D*(r+2));
 gamma2 = h*h*a/(zeta2*D*(r+2))*(phi1+R*psi1);
 psi2 = -(...
     g1/D*(a*D*(phi1+b1)-a*b0*y2-(a+y2)*phi0)+(a*g1-omega1*(r+1))*psi1)/...
@@ -51,14 +51,14 @@ b2 = ...
     (a*a*psi2-a*psi1-(lambda1+C2*l2*(r+1))*b1+(lambda2-C2*l3*r)*b0)...
     /(C2*l1*(r+2)-lambda0);
 
-phi3 = R*(a*h/(z2*D))^2*(1/(r+2)/(r+3));
+phi3 = a*gamma2/(zeta2*D*(r+3));
 gamma3 = ...
     (zeta2*y2*(r+2)*gamma2+...
     h*h*a*(phi2+R*psi2)...
     -h*h*(phi1+R*psi1)-R*g1*a/C1*gamma2)/...
     (zeta2*D*(r+3));
 
-phi4 = (z2*y2*(r+3)*phi3+a*gamma3-gamma2)/(z2*D*(r+4));
+phi4 = (zeta2*y2*(r+3)*phi3+a*gamma3-gamma2)/(zeta2*D*(r+4));
 
 
 Psi = t.^r.*(psi0 + t.*(psi1+t.*psi2));
