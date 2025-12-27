0461770826f950651c6317a94c69aca130df332d
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-12-27 10:25:04 +0300

    main file created

---
+++ src/bidisp/main_full_eigenfunc.m
@@ -0,0 +1,52 @@
+clc
+clear all
+close all
+
+global sigma_fig sigma_max_limit sigma_min_limit q xBarLeft xBarRight
+sigma_fig = 9;
+sigma_max_limit = -1;
+sigma_min_limit = -3;
+q = 1.003;
+xBarLeft = 3e-3;
+xBarRight = 0;
+%% packed bed params
+% a0 = 0.2;
+% alpha = [0, 0.1, 0.3, 0.5, 0.7];
+% marker = ['o','s','d','*','x'];
+%
+% params = poly_case(a0, 0.3, (a0*a0/2)*1.1005);
+% params.marker = marker(2);
+%
+% N = 150;
+% % q = 1.06;
+% % mesh = log_mesh(q, N, params);
+% mesh = quasiuniform_mesh_Frobenius(1e-16, N, params);
+% starter = @(sigma, params) starter_R_zero_X_Psi(...
+%     sigma, params, mesh);
+
+alpha = 0.4;
+a0 = 0.1;
+tau0 = 0.4;
+R = 0;
+sigma = -1.821098878268054;
+
+params = poly_case(a0, alpha, tau0, R);
+mesh = set_left_mesh(300, params, xBarLeft);
+
+% solver = @(problem, mesh) solver_KellerBox(problem, mesh);
+% solver = @(problem, mesh) solver_BVP(problem, mesh, params);
+
+solver = @(problem, mesh) solver_RK(problem, mesh, params);
+starter = @(sigma, params) starter_full(...
+    solver, sigma, params, mesh);
+sol = starter(sigma, params);
+% sol.y(end,2)
+pen = set_pen('b', '--');
+plot_solution(pen, params, sol)
+accuracy = (sol.condition/sol.y(sol.id,1)-1)
+
+xi0 = params.z0/params.z2;
+v = -params.g0/params.C1*(params.C2 + (1+sigma)*(1-xi0));
+figure(701)
+plot(xi0, v, 'd')
+
