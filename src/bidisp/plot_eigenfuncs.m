d59adfac9a0003eb2a504d97c74b2c2c9e74f48c
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-12-28 20:37:48 +0300

    new cases for calculation

--- src/bidisp/plot_eigenfuncs.m
+++ src/bidisp/plot_eigenfuncs.m
@@ -1,8 +1,8 @@
 clc
-close all
+% close all
 clear all
 
-[params, sigma] = case_1();
+[params, sigma] = case_5();
 N = 7001;
 mesh = quasiuniform_mesh(N, params);
 % mesh = log_mesh(1.0005, N, params);
@@ -45,4 +45,52 @@ params.z0 = z0(params);
 params.z2 = z2(params);
 
 sigma = -2.061672191741087;
+end
+
+function [params, sigma] = case_3
+params.a0 = 0.3;
+params.a1 = 1.0;
+params.r = 0.5; % dust volume fraction
+params.g1 = (1-params.r)/params.a1;
+params.g0 = params.g1 + params.r/params.a0;
+params.R = 2;
+params.h = 5;
+params.t = 0.4;
+params.a = sqrt(2*params.t);
+params.z0 = z0(params);
+params.z2 = z2(params);
+
+sigma = 6.45;
+end
+
+function [params, sigma] = case_4
+params.a0 = 0.3;
+params.a1 = 1.0;
+params.r = 0.5; % dust volume fraction
+params.g1 = (1-params.r)/params.a1;
+params.g0 = params.g1 + params.r/params.a0;
+params.R = 0.1;
+params.h = 5;
+params.t = 0.4;
+params.a = sqrt(2*params.t);
+params.z0 = z0(params);
+params.z2 = z2(params);
+
+sigma = -1.3212;
+end
+
+function [params, sigma] = case_5
+params.a0 = 0.3;
+params.a1 = 1.0;
+params.r = 0.5; % dust volume fraction
+params.g1 = (1-params.r)/params.a1;
+params.g0 = params.g1 + params.r/params.a0;
+params.R = 0.1;
+params.h = 50;
+params.t = 0.4;
+params.a = sqrt(2*params.t);
+params.z0 = z0(params);
+params.z2 = z2(params);
+
+sigma = -1.0;
 end
