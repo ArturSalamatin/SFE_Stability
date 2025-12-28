8876f773f739a5bedfbf18bc870d1946b4a98f9c
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2025-12-28 13:58:50 +0300

    another case

--- src/bidisp/plot_eigenfuncs.m
+++ src/bidisp/plot_eigenfuncs.m
@@ -2,16 +2,17 @@ clc
 % close all
 clear all
 
-[params, sigma] = case_1();
+[params, sigma] = case_2();
 N = 7001;
 mesh = quasiuniform_mesh(N, params);
 % mesh = log_mesh(1.0005, N, params);
 
 sol = starter_Y(sigma, params, mesh);
 
-pen.lc = 'r';
+pen.lc = 'b';
 pen.style = '-';
 plot_solution(sol.t,sol.y,pen);
+
 
 
 function [params, sigma] = case_1
@@ -28,4 +29,20 @@ params.z0 = z0(params);
 params.z2 = z2(params);
 
 sigma = -1.62442043;
+end
+
+function [params, sigma] = case_2
+params.a0 = 0.01;
+params.a1 = 1.0;
+params.r = 0.2; % dust volume fraction
+params.g1 = (1-params.r)/params.a1;
+params.g0 = params.g1 + params.r/params.a0;
+params.R = 0.0001;
+params.h = 5;
+params.t = 0.47;
+params.a = sqrt(2*params.t);
+params.z0 = z0(params);
+params.z2 = z2(params);
+
+sigma = -2.061672191741087;
 end
