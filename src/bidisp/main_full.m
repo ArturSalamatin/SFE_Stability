2c38390375a32c1871204ea6fcf0a5216c6060af
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-01-03 11:14:39 +0300

    refactored

--- src/bidisp/main_full.m
+++ src/bidisp/main_full.m
@@ -26,11 +26,15 @@ for i = 1:numel(Tau0_vals)
     tau0 = Tau0_vals(i);
     num = 5200+tau0*100;
     my_figure(num)
+    hold on
     set(gcf, 'units', 'centimeters', 'OuterPosition', [10.42 6.27 8.5 9])
     set(gca, 'FontSize', 10, 'Position', [0.16, 0.2, 0.78, 0.74])
-    ylabel(['{\it\sigma}_0, ' char(8211)])
-    xlabel(['{\itR}_0, ' char(8211)])
+    ylabel(['{\it\sigma}, ' char(8211)])
+    xlabel(['{\itR}, ' char(8211)])
     
+    Sigma = zeros(numel(Alpha_vals), numel(R_vals), numel(h_vals));
+%     load(['full_data/tau_', num2str(tau0*100), '.mat'], ...
+%         'tau0', 'A0_vals', 'Sigma', 'Alpha_vals', 'R_vals', 'h_vals')
     for j = 1:numel(Alpha_vals)
         alpha = Alpha_vals(j);
         
@@ -38,14 +42,14 @@ for i = 1:numel(Tau0_vals)
         style = {'-'};
         pen = set_pen(col, style);
         %% run calculations
-        [tau0, a0, sigma] = ...
+        Sigma(j,:,:) = ...
             calc_sigma_full(solver, N, alpha, tau0, a0, R_vals, h_vals, pen);
         
-        plot(R_vals, sigma, 'r')
+        plot(R_vals, sigma, 'k--')
         
-        %     save(['full_data/alpha_', num2str(alpha*100), '.mat'], ...
-        %         'tau0', 'a0', 'sigma', 'alpha')
     end
+    save(['full_data/tau_', num2str(tau0*100), '.mat'], ...
+        'tau0', 'A0_vals', 'Sigma', 'Alpha_vals', 'R_vals', 'h_vals')
 end
 
 
