358cfda51d38f2ee7f2af0ea963c79ee956ec323
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-02-26 09:48:04 +0300

    set the structure with problem parameters

---
+++ src/bidisp/solver2D/set_params.m
@@ -0,0 +1,17 @@
+function params = set_params(a0, alpha, B, H)
+% size of dust particles
+params.a0 = a0;
+% volume fraction of dust particles
+params.r = alpha; % dust volume fraction
+% size of large particles
+params.a1 = 1.0;
+% SSA of large particles
+params.g1 = (1-params.r)/params.a1;
+% overall SSA
+params.g0 = params.g1 + params.r/params.a0;
+% ratio of two spatial scales
+params.B = B;
+% dimensionless vessel height
+params.H = H;
+end
+
