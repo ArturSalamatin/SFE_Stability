5e9f6868311217409badc6a72d2026207dc1b33e
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-03-01 11:49:01 +0300

    fix

--- src/bidisp/solver2D/single_iteration.m
+++ src/bidisp/solver2D/single_iteration.m
@@ -156,7 +156,7 @@ shift = 4*mesh.size;
 L = linear_index(J, I, mesh);
 L = L(:);
 rhs(L + shift) = -(y(:) - x(:).*x(:)/2);
-%% problem atrix
+%% problem matrix
 m = eq_nmbr*mesh.size;
 nnz = 7*mesh.size + 3*5*mesh.size;
 A = sparse([],[],[],m,m,nnz);
