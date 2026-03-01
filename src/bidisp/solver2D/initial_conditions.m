b3e32c6df3e0ad0f196ad4c95e949a9e6b853755
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-03-01 16:50:25 +0300

    fix initial conditions

--- src/bidisp/solver2D/initial_conditions.m
+++ src/bidisp/solver2D/initial_conditions.m
@@ -2,9 +2,11 @@ function IC = initial_conditions(mesh, params)
 %INITIAL_CONDITIONS Summary of this function goes here
 %   Detailed explanation goes here
 
-IC.p = zeros(mesh.Nz, mesh.Nr);
-IC.c = zeros(mesh.Nz, mesh.Nr);
-IC.x = zeros(mesh.Nz, mesh.Nr);
+IC.p = (params.H*ones(1,mesh.Nr) - mesh.z);
+IC.p(end,:) = 0;
+IC.c = ones(mesh.Nz, mesh.Nr);
+IC.c(1,:) = 0;
+IC.x = zeros(mesh.Nz, mesh.Nr)+1e-10;
 IC.y = IC.x.*IC.x/2;
 IC.G = params.g0*IC.x;
 end
