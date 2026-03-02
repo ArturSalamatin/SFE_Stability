d63335316deb1f10bdffc0336577684e94e2c90f
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-03-02 19:36:15 +0300

    plotters are used

--- src/bidisp/solver2D/main.m
+++ src/bidisp/solver2D/main.m
@@ -8,53 +8,19 @@ clear all
 global x_tol
 x_tol = 1e-10;
 %% set parameters
-H = 1;
-B = 1;
-R = 0.0;
+H = 1.5;
+B = 200;
+R = 1.8;
 a0 = 0.1;
-alpha = 0.5;
-Nr = 2;
-Nz = 465;
-dt = 0.003;
+alpha = 0.3;
+Nr = 10;
+Nz = 50;
+dt = 0.002;
 T = 0.5;
 params = set_params(a0, alpha, R, B, H);
 %% set mesh
 mesh = set_mesh(Nr, Nz, dt, T, params);
 
 states = solver2D(mesh, params);
-
-L = 1:(mesh.size)/mesh.Nr;
-p = states(0*mesh.size+L,:);
-c = states(1*mesh.size+L,:);
-y = states(2*mesh.size+L,:);
-G = states(3*mesh.size+L,:);
-x = states(4*mesh.size+L,:);
-
-if(max(max(abs(y - x.*x/2))) > 1e-10)
-    error("y is incorrect!");
-end
-if(max(max(abs(G_of_x(x, params) - G))) > 1e-10)
-    error("G is incorrect!");
-end
-
-
-figure(1001)
-hold on
-plot(mesh.z, x(:, end), 'k')
-axis([0 H 0 1])
-
-z = z_of_x(x(:,end), params, T);
-plot(z, x(:, end), 'r')
-axis([0 H 0 1])
-
-figure(1002)
-hold on
-plot(mesh.z, c(:, end), 'k')
-axis([0 H 0 1])
-
-z = z_of_x(x(:,end), params, T);
-C = c_of_x(x(:,end), params, T);
-plot(z, C, 'r')
-axis([0 H 0 1])
-
-
+plotter_2D(states, mesh);
+plotter_1D(states, mesh, params);
