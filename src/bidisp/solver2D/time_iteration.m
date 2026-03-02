8c0690357afea53c989598ca0172f780a5c88030
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-03-02 18:24:21 +0300

    debug lines commented

--- src/bidisp/solver2D/time_iteration.m
+++ src/bidisp/solver2D/time_iteration.m
@@ -4,6 +4,7 @@ function [state_s, s] = time_iteration(...
 global x_tol
 
 Nz = mesh.Nz;
+Nr = mesh.Nr;
 
 state_s = state_prev;
 
@@ -15,17 +16,17 @@ x_id = 4*mesh.size + (1:mesh.size)';
 
 state_s(y_id) = state_s(y_id) + dt*(1-state_s(c_id));
 state_s(x_id) = sqrt(2*state_s(y_id));
-ll = 3*mesh.size + (1:Nz:mesh.size)';
+% ll = reshape((1:mesh.size)',Nz,Nr);
 state_s(G_id) = G_of_x(state_s(x_id), params);
-% state_s(G_id) = G_of_x(state_s(x_id), params);
 
 s = 0;
 while true
-%     x = state_s(4*mesh.size + (1:mesh.size/2)');
-%     G = state_s(3*mesh.size + (1:mesh.size/2)');
-%     y = state_s(2*mesh.size + (1:mesh.size/2)');
-%     c = state_s(1*mesh.size + (1:mesh.size/2)');
-%     p = state_s(0*mesh.size + (1:mesh.size/2)');
+    
+%     x = state_s(4*mesh.size + ll);
+%     G = state_s(3*mesh.size + ll);
+%     y = state_s(2*mesh.size + ll);
+%     c = state_s(1*mesh.size + ll);
+%     p = state_s(0*mesh.size + ll);
     
     d_s = single_iteration(...
         state_s, state_prev, mesh, params, dt);
@@ -34,11 +35,11 @@ while true
 %         min(d_s(y_id), dt/5)...) ...
 %         ;
     
-%     d_x = d_s(4*mesh.size + (1:mesh.size/2)');
-%     d_G = d_s(3*mesh.size + (1:mesh.size/2)');
-%     d_y = d_s(2*mesh.size + (1:mesh.size/2)');
-%     d_c = d_s(1*mesh.size + (1:mesh.size/2)');
-%     d_p = d_s(0*mesh.size + (1:mesh.size/2)');
+%     d_x = d_s(4*mesh.size + ll);
+%     d_G = d_s(3*mesh.size + ll);
+%     d_y = d_s(2*mesh.size + ll);
+%     d_c = d_s(1*mesh.size + ll);
+%     d_p = d_s(0*mesh.size + ll);
     
     d_s(x_id) = min(dt, max(d_s(x_id), -state_s(x_id)));
     d_s(y_id) = max(d_s(y_id), -state_s(y_id));
@@ -49,11 +50,11 @@ while true
     
     state_s = state_s + d_s;
     
-%     x = state_s(4*mesh.size + (1:mesh.size/2)');
-%     G = state_s(3*mesh.size + (1:mesh.size/2)');
-%     y = state_s(2*mesh.size + (1:mesh.size/2)');
-%     c = state_s(1*mesh.size + (1:mesh.size/2)');
-%     p = state_s(0*mesh.size + (1:mesh.size/2)');
+%     x = state_s(4*mesh.size + ll);
+%     G = state_s(3*mesh.size + ll);
+%     y = state_s(2*mesh.size + ll);
+%     c = state_s(1*mesh.size + ll);
+%     p = state_s(0*mesh.size + ll);
     
     
     state_s(y_id) = max(x_tol*x_tol/2, state_s(y_id));
@@ -64,13 +65,14 @@ while true
     state_s(c_id) = max(0, state_s(c_id));
     state_s(c_id) = min(1, state_s(c_id));
     
-%     x = state_s(4*mesh.size + (1:mesh.size/2)');
-%     G = state_s(3*mesh.size + (1:mesh.size/2)');
-%     y = state_s(2*mesh.size + (1:mesh.size/2)');
-%     c = state_s(1*mesh.size + (1:mesh.size/2)');
-%     p = state_s(0*mesh.size + (1:mesh.size/2)');
+%     x = state_s(4*mesh.size + ll);
+%     G = state_s(3*mesh.size + ll);
+%     y = state_s(2*mesh.size + ll);
+%     c = state_s(1*mesh.size + ll);
+%     p = state_s(0*mesh.size + ll);
     
     s = s+1;
+%     sum(abs(d_s))
     if(sum(abs(d_s)) < 1e-10)
 %         s
         break;
