29a4bdcb4d6ca1d12cb34846c9ce48a05c7153d0
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-03-31 11:16:02 +0300

    comments in set_mesh

--- src/bidisp/solver2D/set_mesh.m
+++ src/bidisp/solver2D/set_mesh.m
@@ -25,13 +25,18 @@ mesh.dz = mesh.z(2)-mesh.z(1);
 mesh.dt = mesh.t(2) - mesh.t(1);
 
 r_mid = [mesh.r(1), (mesh.r(mesh.I_l)+mesh.r(mesh.I_r))/2, mesh.r(end)];
-mesh.r_area = pi*(r_mid(2:end).^2 - r_mid(1:end-1).^2);
+dr = (r_mid(2:end) - r_mid(1:end-1));
+mesh.r_area = pi*(r_mid(2:end).^2 - r_mid(1:end-1).^2)...
+    ...dr
+;
 z_mid = [mesh.z(1); (mesh.z(mesh.J_l)+mesh.z(mesh.J_r))/2; mesh.z(end)];
 dz = (z_mid(2:end) - z_mid(1:end-1));
 mesh.V = mesh.r_area.*dz;
 % mesh.V([1,end],:) = mesh.V([1,end],:)/2;
 % mesh.V(:,[1,end]) = mesh.V(:,[1,end])/2;
-mesh.z_area = 2*pi*dz.*r_mid(2:end-1);
+mesh.z_area = 2*pi*dz.*r_mid(2:end-1) ...
+    ...dz
+;
 
 mesh.size = Nr*Nz;
 
