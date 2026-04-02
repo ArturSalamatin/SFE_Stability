02dfdfcb8b397148171b69245d146fe92acafa30
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-04-02 16:46:10 +0300

    g is defined

---
+++ src/IV_bidisp/g.m
@@ -0,0 +1,23 @@
+function out = g(x, params)
+
+g0 = params.g0;
+g1 = params.g1;
+a0 = params.a0;
+a1 = params.a1;
+
+out = zeros(size(x));
+
+for i = 1:numel(out)
+    if(x(i) < 0)
+        error('X must be positive!');
+    end
+    
+    if(x(i) <= a0)
+        out(i) = g0;
+    elseif(x(i) <= a1)
+        out(i) = g1;
+    else
+        out(i) = 0;
+    end
+end
+end
