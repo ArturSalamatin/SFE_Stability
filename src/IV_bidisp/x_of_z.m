bdfadec0c4620c35d2ae271c023782fbc76878d6
Artur Salamatin <43352868+ArturSalamatin@users.noreply.github.com>
2026-04-02 19:36:04 +0300

    fix

--- src/IV_bidisp/x_of_z.m
+++ src/IV_bidisp/x_of_z.m
@@ -1,32 +1,43 @@
 function out = x_of_z(z, t, params)
 % inverse function for z(x)
-z0 = params.z0;
-a = sqrt(2*t);
 a0 = params.a0;
 g0 = params.g0;
 g1 = params.g1;
 r = params.r;
 
-if(a < a0)
-    % at initial time frame the dependence
-    % is linear. The inversion is simple
-    out = a - g0*z;
-else
-    out = zeros(size(z));
-    
-    for i = 1:numel(z)
-        if(z(i) > z0)
+tn = numel(t);
+zn = numel(z);
+
+[t, z] = meshgrid(t,z);
+
+out = zeros(size(z));
+
+parfor ti = 1:tn
+    Z0 = z0(t(1,ti),params);
+    Z2 = z2(t(1,ti),params);
+    for zj = 1:zn
+        A = a(t(zj,ti));
+        if(z(zj,ti) > Z2)
+            out(zj,ti) = 0;
+            continue;
+        end
+        % z < z2
+        if(A < a0)
+            % at initial time frame the dependence
+            % is linear. The inversion is simple
+            out(zj,ti) = A - g0*z(zj,ti);
+        else
             % a > a0
-            % dependence is also linear
-            out(i) = a0 - g0*z(i) + ...
-                g0/g1*((a - a0 + r/(1-r)*log((r+(1-r)*a0)/(r+(1-r)*a))));
-        elseif(z == 0)
-            out(i) = a;
-        else % 0 < z < z0
-            % non-linear dependence
-            I = find(params.z > z(i), 1, 'last');
-            x_guess = (params.x(I) + params.x(I+1))/2;
-            out(i) = fzero(@(X) fun(X, a, z(i), r), x_guess);
+            if(z(zj,ti) > Z0)
+                % dependence is also linear
+                out(zj,ti) = a0 - g0*z(zj,ti) + ...
+                    g0/g1*((A - a0 + r/(1-r)*log((r+(1-r)*a0)/(r+(1-r)*A))));
+            elseif(z(zj,ti) == 0)
+                out(zj,ti) = A;
+            else % 0 < z < z0
+                % non-linear dependence
+                out(zj,ti) = fzero(@(X) fun(X, A, z(zj,ti), r), [a0, A]);
+            end
         end
     end
 end
